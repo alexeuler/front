9db9d15467ec367dd0a2aa4000a01012ae8c29ce
alleycat@laptop <karasev.alexey@gmail.com>
2014-04-09 01:50:37 +0400

    finished posts view

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,15 @@ class Post < ActiveRecord::Base
   def self.ai_picked
     self.order(likes_count: :desc).limit(10)
   end
+  
+  def attachment_vk_url
+    attachment_type == "video" ? "http://vk.com/wall#{owner_id}_#{vk_id}"\
+            "?z=#{attachment_type}#{attachment_owner_id}_#{attachment_id}" :
+            "http://vk.com/#{attachment_type}#{attachment_owner_id}_#{attachment_id}"
+  end
+
+  def vk_url
+    "http://vk.com/wall#{owner_id}_#{vk_id}"
+  end
 
 end
