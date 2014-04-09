bc52b8cffa50363d81c1131bdd741c233d83d2cf
alleycat@laptop <karasev.alexey@gmail.com>
2014-04-10 00:13:42 +0400

    added profile info

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
+    redirect_to root_path unless user_signed_in?
     @posts = Post.ai_picked
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def url
+    uid = email.split("@")[0]
+    "http://vk.com/id#{uid}"
+  end
+
 end
