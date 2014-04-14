6c6e3d85dd1a2d5630b8a2b27cad93d7879a04ca
alleycat@laptop <karasev.alexey@gmail.com>
2014-04-15 00:46:20 +0400

    finished featureable spec

--- lib/svm/featureable.rb
+++ lib/svm/featureable.rb
@@ -11,18 +11,17 @@ module SVM
     end
 
     def to_feature
-      feature_ordinals,  feature_names, feature_lambdas = self.class.feature_ordinals,
-          self.class.feature_names, self.class.feature_lambdas
-      names = feature_ordinals.nil? ? feature_names : feature_names - feature_ordinals
+      feature_ordinals = self.class.feature_ordinals
+      feature_names= self.class.feature_names && self.class.feature_names.map(&:to_sym)
+      feature_lambdas = self.class.feature_lambdas
+      names = feature_ordinals.nil? ? feature_names : feature_names - feature_ordinals.keys.map(&:to_sym)
       ary = []
       names.each do |name|
-        name = name.to_sym
         value = self.send(name)
         value = feature_lambdas[name].call(value) if feature_lambdas and feature_lambdas.keys.include?(name)
         ary << value
       end
       feature_ordinals.each_pair do |name, states|
-        name = name.to_sym
         value = self.send(name)
         index = states.index(value)
         vector = Array.new(states.count, 0)
--- spec/svm/featureable_spec.rb
+++ spec/svm/featureable_spec.rb
@@ -9,13 +9,54 @@ module SVM
           include Featureable
           self.feature_names = %w(velocity x_coord y_coord)
         end
-
         test = FeatureTest.new
         test.velocity = 0.5
         test.x_coord = 1
         test.y_coord = 0
         expect(test.to_feature).to eq([0.5, 1, 0])
       end
+
+      it "transforms feature_names into vector, using feature_lambdas" do
+        class FeatureTest < OpenStruct
+          include Featureable
+          self.feature_names = %w(velocity x_coord y_coord)
+          self.feature_lambdas = {
+              x_coord: lambda {|x| x + 1}
+          }
+        end
+        test = FeatureTest.new
+        test.velocity = 0.5
+        test.x_coord = 1
+        test.y_coord = 0
+        expect(test.to_feature).to eq([0.5, 2, 0])
+      end
+
+      it "transforms feature_ordinals into 0-1 vector" do
+        class FeatureTest < OpenStruct
+          include Featureable
+          self.feature_names = %w(velocity x_coord y_coord status)
+          self.feature_lambdas = {
+              x_coord: lambda {|x| x + 1}
+          }
+          self.feature_ordinals = {
+              status: [nil, "on", "off"]
+          }
+        end
+        test = FeatureTest.new
+        test.velocity = 0.5
+        test.x_coord = 1
+        test.y_coord = 0
+        expect(test.to_feature).to eq([0.5, 2, 0, 1, 0, 0])
+
+        test.velocity = 0.5
+        test.x_coord = 1
+        test.y_coord = 0
+        test.status = "off"
+        expect(test.to_feature).to eq([0.5, 2, 0, 0, 0, 1])
+
+      end
+
+
     end
 
   end
