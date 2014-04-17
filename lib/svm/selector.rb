22d423fe9699c4112d9fa4b810571f232e8f0145
alleycat@laptop <karasev.alexey@gmail.com>
2014-04-18 01:28:03 +0400

    modified select method

--- lib/svm/selector.rb
+++ lib/svm/selector.rb
@@ -8,27 +8,38 @@ module SVM
 
     def self.select(user)
       labels, posts = self.get_training_sample(user)
-      #ToDo - add logic if training set is empty
+
+      if labels.count == 0
+        sample = get_untrained_sample
+        items = pick_random_items(sample, POSTS_PER_PAGE)
+        ids = items.map(&:id)
+        return Post.includes(:post_like).where(id: ids).to_a
+      end
+
       svm = Svm.new user: user
       svm.train(labels, posts)
-      svm.save
-      trained_ids = posts.map(&:id)
-      untrained = trained_ids.empty? ? Post.all.to_a : Post.where("id not in (?)", trained_ids).to_a
-      predicted = untrained.map {|p| [svm.predict_probability(p), p.id]}
-      predicted.sort!
-      svm_posts_number = ([posts.count.to_f / MAX_TRAINING, 1].min * POSTS_PER_PAGE).round(0)
-      #TODO add posts count limit here
+
+      untrained_sample = get_untrained_sample(posts)
+      probabilities = untrained_sample.map { |p| [svm.predict_probability(p), p.id] }
+      probabilities.sort!
+      suggested_posts_number = ([posts.count.to_f / MAX_TRAINING, 1].min * POSTS_PER_PAGE).round(0)
+      result_count = POSTS_PER_PAGE
       result = []
-      svm_posts_number.times {result << predicted.pop}
-      predicted.shuffle!
-      (POSTS_PER_PAGE - svm_posts_number).times do
-        item = predicted.pop
-        like = PostLike.where(user_id: user.id, post_id: item[1]).first_or_initialize
+      svm_posts_number.times do
+        break if probabilities.count == 0
+        result << predicted.pop
+        result_count -= 1
+      end
+
+      result += self.pick_random_items(predicted, result_count)
+
+      ids = result.map { |x| x[1] }
+      ids.each do |id|
+        like = PostLike.where(user_id: user.id, post_id: id).first_or_initialize
         like.value = 0
         like.save
-        result << item
       end
-      ids = result.map {|x| x[1]}
+
       Post.includes(:post_like).where(id: ids).to_a
     end
 
@@ -36,11 +47,33 @@ module SVM
 
     def self.get_training_sample(user)
       posts = Post.includes(:post_like).where(post_likes: {user_id: user.id}).to_a
-      #Post.includes(:post_like).where('"post_likes".user_id = ?', user.id).to_a
-      labels = posts.map {|p| p.post_like.value}
+      labels = posts.map { |p| p.post_like.value }
       [labels, posts]
     end
 
+    def self.get_untrained_sample(trained_sample = [])
+      return Post.all.to_a if trained_sample.nil? or trained_sample.empty?
+      trained_ids = trained_sample.map(&:id)
+      Post.where("id not in (?)", trained_ids).to_a
+    end
+
+    def self.get_untrained_sample_ids(trained_sample = [])
+      return Post.select(:id).all.to_a if trained_sample.nil? or trained_sample.empty?
+      trained_ids = trained_sample.map(&:id)
+      Post.where("id not in (?)", trained_ids).to_a
+    end
+
+
+    def self.pick_random_items(array, n)
+      array.shuffle!
+      result = []
+      n.times do
+        break if array.count == 0
+        item = array.pop
+        result << item
+      end
+      result
+    end
 
   end
 
