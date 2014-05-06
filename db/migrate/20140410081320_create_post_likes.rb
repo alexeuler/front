af784026778763497cbe8e93ae44076df3467ff4
alexey <karasev.alexey@gmail.com>
2014-05-06 15:33:12 +0400

    modified models

--- db/migrate/20140410081320_create_post_likes.rb
+++
@@ -1,9 +0,0 @@
-class CreatePostLikes < ActiveRecord::Migration
-  def change
-    create_table :post_likes do |t|
-      t.integer :post_id
-      t.integer :user_id
-      t.integer :value
-    end
-  end
-end
