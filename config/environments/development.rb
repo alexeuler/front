ab78e29400f94cbc87d56530fe95eac3cbeb92ac
alleycat@laptop <karasev.alexey@gmail.com>
2014-05-13 00:53:38 +0400

    moved redis conntection into development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,9 @@
+require 'redis'
+$redis = Redis.new(:host => ENV["FRONT_DB_HOST"], :port => ENV["FRONT_DB_PORT"],
+                   :db => ENV["FRONT_DB_NAME"])
+$redis.auth(ENV["FRONT_DB_PASS"])
+
+
 Front::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
