6a6ac3b655cf6728575c03f644138a9d43946537
alleycat@laptop <karasev.alexey@gmail.com>
2014-05-13 01:05:48 +0400

    moved redis back

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,3 @@
-require 'redis'
-$redis = Redis.new(:host => ENV["FRONT_DB_HOST"], :port => ENV["FRONT_DB_PORT"],
-                   :db => ENV["FRONT_DB_NAME"])
-$redis.auth(ENV["FRONT_DB_PASS"])
 
 
 Front::Application.configure do
