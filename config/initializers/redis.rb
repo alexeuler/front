9d4a3dd48accb6ce8b0fe09d695ce91485807cbb
alleycat@laptop <karasev.alexey@gmail.com>
2014-05-13 01:11:03 +0400

    moved redis back

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 require 'redis'
 $redis = Redis.new(:host => ENV["FRONT_DB_HOST"], :port => ENV["FRONT_DB_PORT"],
                    :db => ENV["FRONT_DB_NAME"])
-puts "-------------Path: #{ENV["FRONT_DB_PASS"]}"
+puts Hash.new(:host => ENV["FRONT_DB_HOST"], :port => ENV["FRONT_DB_PORT"],
+                   :db => ENV["FRONT_DB_NAME"])
 $redis.auth(ENV["FRONT_DB_PASS"])
