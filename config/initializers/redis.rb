0597796d8704414eda7d4a27c0a57b2736e2ce81
alexey <karasev.alexey@gmail.com>
2014-05-13 14:09:19 +0400

    test

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,6 +2,5 @@ require 'redis'
 params = {:host => ENV["FRONT_DB_HOST"], :port => ENV["FRONT_DB_PORT"],
           :db => ENV["FRONT_DB_NAME"]}
 $redis = Redis.new(params)
-puts params
-puts $redis.nil?
-#$redis.auth(ENV["FRONT_DB_PASS"])
+puts ENV["FRONT_DB_PASS"]
+$redis.auth(ENV["FRONT_DB_PASS"])
