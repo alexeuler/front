e21e35f14c1285775a7a106c74fad4f1b806bd59
alexey <karasev.alexey@gmail.com>
2014-05-14 15:08:08 +0400

    deleted redis auth

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,5 @@
 require 'redis'
 params = {:host => ENV["FRONT_DB_HOST"], :port => ENV["FRONT_DB_PORT"],
           :db => ENV["FRONT_DB_NAME"]}
-begin
 $redis = Redis.new(params)
-rescue Exception=>e
-  puts "Unable to connect to Redis: #{e.message}"
-end
 
