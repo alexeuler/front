ed3900286c6c5fb7fa870e10a1561aa428aaf693
alexey <karasev.alexey@gmail.com>
2014-05-14 15:07:22 +0400

    minor refactoring

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,6 @@ params = {:host => ENV["FRONT_DB_HOST"], :port => ENV["FRONT_DB_PORT"],
           :db => ENV["FRONT_DB_NAME"]}
 begin
 $redis = Redis.new(params)
-$redis.auth(ENV["FRONT_DB_PASS"])
 rescue Exception=>e
   puts "Unable to connect to Redis: #{e.message}"
 end
